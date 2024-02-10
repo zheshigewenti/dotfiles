fedff5d28b48df5eeadbcb43d38e6f7349228844
zheshigewenti <dzn1534564656@gmail.com>
2024-02-10 20:05:35 +0800

    update

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,5 +1,5 @@
 --luasnip
-require("luasnip.loaders.from_vscode").lazy_load()
+--require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/snip/" } })
 
 
