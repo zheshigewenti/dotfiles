901eaf81812ada41ff8b38e94df37350cf81afa7
vincent <vincent@myheadsoiron.>
2025-06-21 11:41:14 +0800

    update

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,3 +1,3 @@
 require("mason").setup{}
 require("mason-lspconfig").setup{                             --autoinstall lsp
-        ensure_installed = {"lua_ls","clangd","rust_analyzer", "ts_ls" }}
+        ensure_installed = {"lua_ls","clangd"}}
